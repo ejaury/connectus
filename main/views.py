f7e3950d81fd278d77d9786da2d592530218581d
Edwin Jaury <edwinjaury@gmail.com>
2010-02-02 20:30:57 -0800

    Modified Main page to show more appropriate text and class thumbnails.
    - Fixed background size issue.
    - Reduced shadow coarse
    - Replaced toolbar with a conventional rectangular bar

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(req):
-  return render_to_response('main/index.html')  
+  return render_to_response('main/index.html',
+                            context_instance=RequestContext(req))
